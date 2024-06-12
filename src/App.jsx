e335505badce388c783ff1f5512a9bbb6716b4a2
Maryfidlay <mariefidlay@gmail.com>
2024-06-12 11:28:47 -0700

    puppy_data_with_useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
-
-import './App.css'
+import { useState } from "react";
+import { puppyList } from "./data.js";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [puppies, setPuppies] = useState(puppyList);
 
+  console.log("puppyList:", puppyList);
   return (
-    <>
-      <div>
+      <div className="App">
+        {
+        puppies.map((puppy) => {
+          return <p key={puppy.id}>{puppy.name}</p>;
+        })
+      }
       </div>
-
-    </>
-  )
+  );
 }
 
-export default App
+export default App;
